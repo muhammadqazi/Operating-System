da671595d40582fa8af29c519c42000ec8674f95
Muhammad Qazi <tigerbhai888@gmail.com>
2021-12-09 15:40:30 +0200

    do-while user input

--- main.cpp
+++ main.cpp
@@ -1,24 +1,58 @@
- 
-#include<iostream>
- 
+
+#include <iostream>
+#include <limits>
+
 using namespace std;
+
+int Validator(int option)
+{
+    if (option == 1 || option == 2 || option == 3 || option == 4)
+    {
+        return true;
+    }
+    else
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return false;
+    }
+}
 
 int main()
 {
 
-    int a;
-
+    int option;
+    bool validate = true;
     //Ask user to input method
-    
-    cout<<"Please choose from the given options below"<<endl;
-    cout<<"\n 1) Scheduling Method (None)"<<endl;
-    cout<<"\n 2) Preemptive Mode (Off)"<<endl;
-    cout<<"\n 3) Show Result"<<endl;
-    cout<<"\n 4) End Program"<<endl;
+    do
+    {
+        if (validate)
+        {
+            cout << "Please choose from the given options below" << endl;
+        }
 
-    cout<<"Option> ";
-    cin>>a;
-    cout<<a;
-     
+        cout << "\n 1) Scheduling Method (None)" << endl;
+        cout << "\n 2) Preemptive Mode (Off)" << endl;
+        cout << "\n 3) Show Result" << endl;
+        cout << "\n 4) End Program" << endl;
+
+        cout << "Option> ";
+
+        cin >> option;
+
+        //validator return false for not valid data
+        validate = Validator(option);
+
+        if (!validate)
+        {
+            cout << "\n Please enter the correct choice" << endl;
+        }
+
+        //if the data is valid 
+        //the validator will send true i will make it false to break the loop
+        //if the validator send false i will make it true to loop again
+        
+    } while (!validate);
+
     return 0;
 }
