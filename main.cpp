b0924a1a84220452488e11b3c9eab39e59a12ff5
Muhammad Qazi <tigerbhai888@gmail.com>
2021-12-09 15:06:22 +0200

    user-input part

---
+++ main.cpp
@@ -0,0 +1,24 @@
+ 
+#include<iostream>
+ 
+using namespace std;
+
+int main()
+{
+
+    int a;
+
+    //Ask user to input method
+    
+    cout<<"Please choose from the given options below"<<endl;
+    cout<<"\n 1) Scheduling Method (None)"<<endl;
+    cout<<"\n 2) Preemptive Mode (Off)"<<endl;
+    cout<<"\n 3) Show Result"<<endl;
+    cout<<"\n 4) End Program"<<endl;
+
+    cout<<"Option> ";
+    cin>>a;
+    cout<<a;
+     
+    return 0;
+}
